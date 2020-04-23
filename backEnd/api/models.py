21307fe93537cee90cb50412495faebf0dfdc76b
Diego G <diego.gomes87@gmail.com>
2020-04-23 04:06:02 -0300

    bug in requests api

--- backEnd/api/models.py
+++ backEnd/api/models.py
@@ -7,6 +7,24 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
+    def __str__(self):
+        return self.title
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -16,3 +34,6 @@ class Rating(models.Model):
     class Meta:
         unique_together = (('user', 'movie'),)
         index_together = (('user', 'movie'),)
+
+    def __str__(self):
+        return str(self.movie)+' Start: ' + str(self.stars)
--- backEnd/api/urls.py
+++ backEnd/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from api.views import MovieViewSet, RatingViewSet
 
 routers = routers.DefaultRouter()
+routers.register('rating', RatingViewSet)
+routers.register('movies', MovieViewSet)
+
 
 urlpatterns = [
     path('', include(routers.urls)),
